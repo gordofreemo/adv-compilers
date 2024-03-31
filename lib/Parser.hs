4e9dc29345a87520a3b4d367c3559bb3d122f3ce
trlarkin <tristanjlarkin@gmail.com>
2024-03-31 11:54:17 -0600

    added new function to parser

--- lib/Parser.hs
+++ lib/Parser.hs
@@ -14,6 +14,7 @@ import           Text.Parsec.Error
 import           Text.Parsec.Prim                   hiding (label)
 import           Text.Parsec.String                 (Parser)
 import           Typing                             as T
+import qualified ReductionSemantics as RS
 
 
 -- | The actual parser that should be used to parse an entire program
@@ -140,6 +141,10 @@ parseProgram fname = do
         case parse programParser "" fileData of
                 Left err -> error $ show err
                 Right x  -> return x
+
+reductionSem x = do
+        prgm <- parseProgram x
+        return $ RS.makeEvalContext prgm
 
 typeCheckTerm :: S.Term -> S.Type
 typeCheckTerm t = T.typeCheck t
